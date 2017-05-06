c6bcfc40b6a5e93096c3f76726eaeb31c6182bdf
Alex G <AresEyE@163.com>
2017-05-06 17:42:36 +0800

    Read lifenews from server

---
+++ gunicorn/qlive/qlive/newsgetter/news.py
@@ -0,0 +1,51 @@
+#encoding:utf8
+import os, sys, pdb, requests, time, json
+import datetime
+
+def getNews():
+    url = ur'https://wechat.gomass.xyz/polls/getLifeFeed'
+
+    timestamp = str(int(time.time()) * 1000)
+    latitude, longitude = '40.00721', '116.3199'
+    r = requests.post(url, 
+            data = {'user_raw_id': 'appname',
+                'app_request_timestamp':timestamp,
+                'latitude':latitude,
+                'longitude':longitude,},
+            verify=False)
+    lifenews = json.loads(r.text)
+    infoList = list()
+    for ind in xrange(0, len(lifenews['contentList'])):
+        cellInfo = dict()
+        cellInfo['nickname'] = lifenews['nicknameList'][ind]
+        cellInfo['timestamp'] = lifenews['timestampList'][ind]
+        cellInfo['content'] = lifenews['contentList'][ind]
+        cellInfo['lifeimagehash'] = lifenews['lifeImageHashList'][ind]
+        cellInfo['profileimagehash'] = lifenews['profileImageHashList'][ind]
+        cellInfo['wechatprofileimage'] = lifenews['wechat_profileimageList'][ind]
+
+        # Converted info
+        cellInfo['datetime'] = toDatetime(lifenews['timestampList'][ind])
+        cellInfo['htmlcontent'] = htmlProcess(cellInfo['content'])
+        infoList.append(cellInfo)
+
+
+    return infoList
+
+def htmlProcess(text):
+    # htmlcontent = text.replace('\n', '</p><br />\n<p>')
+    paragraphList = text.split('\n')
+    return paragraphList
+
+
+def toDatetime(timestamp):
+    '''Convert unix timestamp to datetime.
+    timestamp: in ms.
+    '''
+    datestr = datetime.datetime.fromtimestamp(int(timestamp) / 1000).strftime('%Y-%m-%d %H:%M:%S')
+    return datestr
+
+if __name__ == '__main__':
+    getNews()
+    # print toDatetime('1493992297473')
+
--- gunicorn/qlive/qlive/views.py
+++ gunicorn/qlive/qlive/views.py
@@ -8,16 +8,14 @@ from django.template import loader
 import json
 import os, sys, logging
 import time
+from newsgetter.news import getNews, toDatetime
 
 
 def index(request):
     '''Default page'''
     template = loader.get_template('html/index.html')
+    infoList = getNews()
     context = {
-        'data_list': [
-            'haha',
-            'gpf',
-            'said',
-            'ok'],
+        'data_list': infoList,
     }
     return HttpResponse(template.render(context, request))
